1f0bbc9b3d143cf02e888f46e6c255ff217c258c
ElisabethFox <lisa.black.fox17@gmail.com>
2023-06-17 13:17:59 +0300

    add button component

---
+++ frontend/src/components/buttton/Button.js
@@ -0,0 +1,6 @@
+const Button = ({ title }) => {
+    return (
+        <button type="submit">{title}</button>
+    )
+};
+export default Button;
--- frontend/src/components/login-form/LoginForm.js
+++ frontend/src/components/login-form/LoginForm.js
@@ -1,29 +1,38 @@
 import { Formik, Form, Field } from 'formik';
+import Button from "../buttton/Button";
 import './style.css';
 
 const LoginForm = () => {
     return ( 
-        <Formik initialValues={{ email: "", password: "" }} >
+        <Formik initialValues={{ name: "", password: "" }}
+                onSubmit={({ setSubmitting }) => {
+                    console.log("Form is validated! Submitting the form...");
+                    setSubmitting(false);
+                }}
+        >
             {() => (
                 <div className="container">
                     <div className="form-card">
                         <Form>
                             <div className="form-group">
-                                <label htmlFor="email">Email</label>
+                                <label htmlFor="name"></label>
                                 <Field
-                                type="email"
-                                name="email"
-                                className="form-control"
+                                    id="name"
+                                    type="text"
+                                    name="name"
+                                    className="form-control"
                                 />
                             </div>
                             <div className="form-group">
-                                <label htmlFor="password">Password</label>
+                                <label htmlFor="password"></label>
                                 <Field
-                                type="password"
-                                name="password"
-                                className="form-control"
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    className="form-control"
                                 />
                             </div>
+                            <Button title="Войти" />
                         </Form>
                     </div>
                 </div>

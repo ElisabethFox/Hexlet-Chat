01fcea780a0456a24398c63d644f1b58d72e5ac1
ElisabethFox <lisa.black.fox17@gmail.com>
2023-06-23 17:52:38 +0300

    add react-bootstrap

--- frontend/src/components/login-form/LoginForm.js
+++ frontend/src/components/login-form/LoginForm.js
@@ -1,5 +1,5 @@
 import Form from 'react-bootstrap/Form';
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import logInSchema from "../../validation/logInShema";
 import {useNavigate} from "react-router-dom";
 import {useFormik} from "formik";
@@ -12,19 +12,23 @@ import './style.css';
 const LoginForm = () => {
     const { logIn } = useContext(UserDataContext);
     const navigate = useNavigate();
+    const [isInvalid, setValid] = useState(false);
 
     const formik = useFormik({
     initialValues: { name: "", password: "" },
     validationSchema: logInSchema,
-    onSubmit: async (val) => {
+    onSubmit: async (values) => {
+        const { name, password } = values;
         try {
+            setValid(false);
             await axios
-                .post('/api/v1/login', { username: '123456678', password: 'admin' })
+                .post('/api/v1/login', { username: name, password: password })
                 .then((response) => {
                     logIn(response);
                     navigate('/');
                 });
         } catch {
+            setValid(true);
             console.log('Неверные имя пользователя или пароль')
         }
     },
@@ -42,15 +46,12 @@ const LoginForm = () => {
                         placeholder="Ваш ник"
                         autoComplete="username"
                         onChange={formik.handleChange}
+                        isInvalid={isInvalid}
                         required
                     />
                     <Form.Label htmlFor="name" className="form-label">
                         Ваш ник
                     </Form.Label>
-                    <Form.Control.Feedback type="invalid" className="invalid-feedback"
-                                           tooltip={formik.errors.name && formik.touched.name}>
-                        Неверные имя пользователя или пароль
-                    </Form.Control.Feedback>
                 </div>
                 <div className="form-floating mb-4">
                     <Form.Control
@@ -61,13 +62,14 @@ const LoginForm = () => {
                         placeholder="Ваш пароль"
                         autoComplete="current-password"
                         onChange={formik.handleChange}
+                        isInvalid={isInvalid}
                         required
                     />
                     <Form.Label htmlFor="password" className="form-label">
                         Пароль
                     </Form.Label>
-                    <Form.Control.Feedback type="invalid" className="invalid-feedback"
-                                           tooltip={formik.errors.password && formik.touched.password}>
+                    <Form.Control.Feedback type="invalid" className="invalid-tooltip invalid-feedback"
+                                           tooltip={isInvalid}>
                         Неверные имя пользователя или пароль
                     </Form.Control.Feedback>
                 </div>

51b257958cf3b63de982df4163b3edab65e0e258
ElisabethFox <lisa.black.fox17@gmail.com>
2023-07-14 16:24:57 +0300

    add channel-menu-btn

---
+++ frontend/src/components/buttons/channel-menu-button/ChannelMenuButton.js
@@ -0,0 +1,10 @@
+const ChannelMenu = () => {
+    return (
+        <>
+            <button type="button" id="react-aria4736936024-3" aria-expanded="false" class="flex-grow-0 dropdown-toggle dropdown-toggle-split btn btn-secondary"><span class="visually-hidden">Управление каналом</span></button>
+            <div x-placement="bottom-end" aria-labelledby="react-aria4736936024-3" class="dropdown-menu" data-popper-reference-hidden="false" data-popper-escaped="false" data-popper-placement="bottom-end" style="position: absolute; inset: 0px 0px auto auto; transform: translate3d(0px, 40px, 0px);"><a data-rr-ui-dropdown-item="" class="dropdown-item" role="button" tabindex="0" href="#">Удалить</a><a data-rr-ui-dropdown-item="" class="dropdown-item" role="button" tabindex="0" href="#">Переименовать</a></div>
+        </>
+    );
+}
+ 
+export default ChannelMenu;
--- frontend/src/components/chat/channel/Channel.js
+++ frontend/src/components/chat/channel/Channel.js
@@ -11,12 +11,23 @@ const Channel = ({ channel, onClick }) => {
         'current': isActive(),
     })
 
+    const channelMenuBtnClasses = cn("flex-grow-0 dropdown-toggle dropdown-toggle-split channel-menu-btn", {
+        'current': isActive(),
+    })
+
     return (
-        <li className="nav-item w-100" key={channel.id}>
+        <li className="nav-item w-100 channel" key={channel.id}>
                 <button type="button" className={channelClasses} onClick={onClick}>
                     <span className="me-1">#</span>
                     {channel.name}
                 </button>
+                <button type="button" id="react-aria4736936024-3" aria-expanded="false" className={channelMenuBtnClasses}>
+                    <span class="visually-hidden">Управление каналом</span>
+                </button>
+                <div x-placement="bottom-end" aria-labelledby="react-aria4736936024-3" class="dropdown-menu" data-popper-reference-hidden="false" data-popper-escaped="false" data-popper-placement="bottom-end">
+                    <a data-rr-ui-dropdown-item="" class="dropdown-item" role="button" tabindex="0" href="#">Удалить</a>
+                    <a data-rr-ui-dropdown-item="" class="dropdown-item" role="button" tabindex="0" href="#">Переименовать</a>
+                </div>
         </li>
     );
 }

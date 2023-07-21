d646a6d933bb81631e530f45be148576c09b9c84
ElisabethFox <lisa.black.fox17@gmail.com>
2023-07-21 21:17:41 +0300

    fix

--- frontend/src/components/chat/channels-panel/ChannelsPanel.jsx
+++ frontend/src/components/chat/channels-panel/ChannelsPanel.jsx
@@ -1,5 +1,4 @@
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { BiMessageSquareAdd } from 'react-icons/bi';
 import Channel from '../channel/Channel';
@@ -22,26 +21,23 @@ const ChannelsPanel = () => {
     dispatch(setCurrentModalType('add'));
     dispatch(openModalWindow());
   };
-    
+
   return (
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b className="channels-title">{t('channel.channels')}</b>
-          <button type="button" className="p-0 border-0 btn btn-group-vertical add-button" data-toggle="modal" onClick={handleCreateNewChannel}>
-            <BiMessageSquareAdd className="add-icon"/>
-            <span className="visually-hidden">{t('channel.addBtn')}</span>
-          </button>
+        <button type="button" className="p-0 border-0 btn btn-group-vertical add-button" data-toggle="modal" onClick={handleCreateNewChannel}>
+          <BiMessageSquareAdd className="add-icon" />
+          <span className="visually-hidden">{t('channel.addBtn')}</span>
+        </button>
       </div>
       <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
-        {channels.map((channel) => {
-          return (
-            <Channel channel={channel} key={channel.id} onClick={() => handleSetCurrentChannel(channel.id)} />
-          )
-        })}
+        {channels.map((channel) =>
+          <Channel channel={channel} key={channel.id} onClick={() => handleSetCurrentChannel(channel.id)} />
+        )};
       </ul>
     </div>
   );
 };
 
 export default ChannelsPanel;
-

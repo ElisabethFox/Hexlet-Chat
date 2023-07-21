9fd0c5385a04a308f13a1c4ce819e3a1c59b583d
ElisabethFox <lisa.black.fox17@gmail.com>
2023-07-21 21:06:23 +0300

    fix

--- frontend/src/components/chat/channel/Channel.jsx
+++ frontend/src/components/chat/channel/Channel.jsx
@@ -37,31 +37,31 @@ const Channel = ({ channel, onClick }) => {
 
   if (!removable) {
     return (
-        <li className="nav-item channel">
-            <button type="button" className={channelClasses} onClick={onClick}>
-                <span className="me-1">{t('channel.prefix')}</span>
-                {name}
-            </button>
-        </li>
-    )
-  };
+      <li className="nav-item channel">
+        <button type="button" className={channelClasses} onClick={onClick}>
+          <span className="me-1">{t('channel.prefix')}</span>
+          {name}
+        </button>
+      </li>
+    );
+  }
 
   return (
     <li className="nav-item channel">
       <Dropdown className="d-flex dropdown btn-group" as={ButtonGroup}>
         <button type="button" className={channelClasses} onClick={onClick}>
           <span className="me-1">{t('channel.prefix')}</span>
-            {name}
-          </button>
+          {name}
+        </button>
 
-      <DropdownToggle variant="channel-menu-btn" type="button" id="dropdown-menu" className={channelMenuBtnClasses}>
+    <DropdownToggle variant="channel-menu-btn" type="button" id="dropdown-menu" className={channelMenuBtnClasses}>
           <span className="visually-hidden">{t('channel.controlChannel')}</span>
-            </DropdownToggle>
+    </DropdownToggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item id="dropdown-item" onClick={() => handleRemoveChannel(id)}>{t('channel.removeChannel')}</Dropdown.Item>
-            <Dropdown.Item id="dropdown-item" onClick={() => handleRenameChannel(id)}>{t('channel.renameChannel')}</Dropdown.Item>
-          </Dropdown.Menu>
+        <Dropdown.Menu>
+          <Dropdown.Item id="dropdown-item" onClick={() => handleRemoveChannel(id)}>{t('channel.removeChannel')}</Dropdown.Item>
+          <Dropdown.Item id="dropdown-item" onClick={() => handleRenameChannel(id)}>{t('channel.renameChannel')}</Dropdown.Item>
+        </Dropdown.Menu>
       </Dropdown>
     </li>
   );

46c60e104f8e9d4c4a566321ff0eaf6118471fcc
ElisabethFox <lisa.black.fox17@gmail.com>
2023-07-21 21:35:24 +0300

    fix

--- frontend/src/components/chat/chat-panel/ChatPanel.jsx
+++ frontend/src/components/chat/chat-panel/ChatPanel.jsx
@@ -8,10 +8,10 @@ const ChatPanel = ({ currentChannelName, currentChannelMessagesCount }) => {
   return (
     <div className="bg-light mb-4 p-3 shadow-sm small">
       <p className="m-0">
-          <b className="channel-name">
-            {t('channel.prefix')} 
-            {currentChannelName}
-          </b>
+        <b className="channel-name">
+          {t('channel.prefix')}
+          {currentChannelName}
+        </b>
       </p>
       <span className="message-count">
         {t('message.messagesCount', { count: currentChannelMessagesCount })}
--- frontend/src/components/login/LoginCard.jsx
+++ frontend/src/components/login/LoginCard.jsx
@@ -5,20 +5,20 @@ import LoginFooter from './LoginFooter';
 import loginImg from '../../img/login.png';
 
 const LoginCard = () => {
-    const { t } = useTranslation();
-    
-    return (
-        <div className="col-12 col-md-8 col-xxl-6">
-            <div className="card shadow-sm">
-                <div className="card-body row p-5">
-                    <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                        <img src={loginImg} alt={t('login.loginTitle')} className="card-img"/>
-                    </div>
-                    <LoginForm />
-                </div>
-                <LoginFooter />
-            </div>
+  const { t } = useTranslation();
+
+  return (
+    <div className="col-12 col-md-8 col-xxl-6">
+      <div className="card shadow-sm">
+        <div className="card-body row p-5">
+          <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+            <img src={loginImg} alt={t('login.loginTitle')} className="card-img"/>
+          </div>
+          <LoginForm />
         </div>
+          <LoginFooter />
+      </div>
+    </div>
     );
 };
 

2961edaa145a535a72ad975bc6db7c1f1d275a7a
ElisabethFox <lisa.black.fox17@gmail.com>
2023-07-21 22:41:44 +0300

    fix

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -10,41 +10,40 @@ import MessageBox from '../components/chat/MessageBox';
 import MessageForm from '../components/chat/MessageForm';
 
 const Chat = () => {
-    const dispatch = useDispatch();
-    const { connectSocket, disconnectSocket, getChannelsData } = useChatApi();
-    const messages = useSelector(messagesSelector.selectAll);
-    const currentChannel = useSelector(currentChannelSelector);
-    const currentChannelName = currentChannel?.name;
-    const currentChannelMessages = messages.filter((message) => message.сhannelId === currentChannel?.id);
-    const currentChannelMessagesCount = currentChannelMessages.length;
+  const dispatch = useDispatch();
+  const { connectSocket, disconnectSocket, getChannelsData } = useChatApi();
+  const messages = useSelector(messagesSelector.selectAll);
+  const currentChannel = useSelector(currentChannelSelector);
+  const currentChannelName = currentChannel?.name;
+  const currentChannelMessages = messages.filter(
+    (message) => message.сhannelId === currentChannel?.id
+  );
+  const currentChannelMessagesCount = currentChannelMessages.length;
 
+useEffect(() => {
+  dispatch(fetchInitialData(getChannelsData));
+  connectSocket();
 
-    useEffect(() => {
-        dispatch(fetchInitialData(getChannelsData));
-        connectSocket();
+  return () => {
+    disconnectSocket();
+  };
+  }, [connectSocket, disconnectSocket, dispatch, getChannelsData]);
 
-        return () => {
-            disconnectSocket();
-          };
-
-    }, [connectSocket, disconnectSocket, dispatch, getChannelsData]);
-
-    return (
-        <div className="container h-100 my-4 overflow-hidden rounded shadow">
-            <div className="row h-100 bg-white flex-md-row">
-                <ModalWindow />
-                <ChannelsPanel />
-            <div className="col p-0 h-100">
-            <div className="d-flex flex-column h-100">
-                <ChatPanel currentChannelName={currentChannelName} currentChannelMessagesCount={currentChannelMessagesCount} />
-                <MessageBox currentChannelMessages={currentChannelMessages} />
-                <MessageForm />
-            </div>
-            </div>
-            </div>
+  return (
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+      <div className="row h-100 bg-white flex-md-row">
+          <ModalWindow />
+          <ChannelsPanel />
+        <div className="col p-0 h-100">
+          <div className="d-flex flex-column h-100">
+            <ChatPanel currentChannelName={currentChannelName} currentChannelMessagesCount={currentChannelMessagesCount} />
+            <MessageBox currentChannelMessages={currentChannelMessages} />
+            <MessageForm />
+          </div>
         </div>
-
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Chat;

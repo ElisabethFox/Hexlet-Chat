99ce70b28fb023b3ca39cf1f4ae6f9967964770b
ElisabethFox <lisa.black.fox17@gmail.com>
2023-07-30 16:09:32 +0300

    fix logout btn

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,12 +1,28 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import LogoutButton from '../buttons/LogoutButton';
 import { appRoutes } from '../../routes';
-
+import { useAuthorization } from '../../hooks';
 import './style.css';
 
 const NavBar = () => {
   const { t } = useTranslation();
+  const { logOut } = useAuthorization();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate(appRoutes.loginPagePath());
+    logOut();
+  };
+
+  const LogoutButton = (handle, title) => {
+    if (localStorage.getItem('user') !== null) {
+      return (
+        <button type="button" className="logout-button" onClick={handle}>{title}</button>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
@@ -14,7 +30,7 @@ const NavBar = () => {
         <NavLink to={appRoutes.chatPagePath()} className="navbar-brand">
           {t('navigation.chatName')}
         </NavLink>
-        <LogoutButton title={t('navigation.exitBtn')} />
+        { LogoutButton(handleLogout, t('navigation.exitBtn')) }
       </div>
     </nav>
   );
